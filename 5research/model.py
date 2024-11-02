d47b2be972936a139aa871fb33ee7c66a2caf86e
AndreeaCZ <andreea.zelko10@gmail.com>
2024-11-02 12:49:52 +0100

    add offensive

--- 5research/model.py
+++ 5research/model.py
@@ -61,7 +61,7 @@ def write_results_to_file(file, report, model_name):
         f.write("\n\n")
 
 
-def train_and_evaluate(lm, X_train, Y_train, X_dev, Y_dev, max_seq_len, learning_rate, batch_size, epochs):
+def train_and_evaluate(lm, X_train, Y_train, X_dev, Y_dev, max_seq_len, learning_rate, batch_size, epochs, extra_features):
     encoder = LabelBinarizer()
     Y_train_bin = encoder.fit_transform(Y_train)
     Y_dev_bin = encoder.transform(Y_dev)
@@ -73,6 +73,21 @@ def train_and_evaluate(lm, X_train, Y_train, X_dev, Y_dev, max_seq_len, learning
     model = TFAutoModelForSequenceClassification.from_pretrained(lm, num_labels=2, from_pt=True)
     tokens_train = tokenizer(X_train, padding=True, max_length=max_seq_len, truncation=True, return_tensors="tf").data
     tokens_dev = tokenizer(X_dev, padding=True, max_length=max_seq_len, truncation=True, return_tensors="tf").data
+
+    for feature in extra_features:
+        func = feature['function']
+        name = feature['name']
+        dtype = feature['dtype']
+        extra_feature_train = [func(tweet) for tweet in X_train]
+        extra_feature_dev = [func(tweet) for tweet in X_dev]
+
+        # Reshape the additional feature
+        extra_feature_tensor = tf.convert_to_tensor(extra_feature_train, dtype=dtype)
+        extra_feature_tensor = tf.convert_to_tensor(extra_feature_dev, dtype=dtype)
+
+        # Append the offensive percentages to the tokens
+        tokens_train[name] = tf.expand_dims(extra_feature_tensor, axis=1)
+        tokens_dev[name] = tf.expand_dims(extra_feature_tensor, axis=1)
 
     loss_function = CategoricalCrossentropy(from_logits=True)
     optim = Adam(learning_rate=learning_rate)
@@ -107,7 +122,8 @@ def main():
         64,
         5e-6,
         128,
-        4
+        4,
+        []
     )
     write_results_to_file('results.txt', report, model_name)
 
---
+++ 5research/off_words_main.py
@@ -0,0 +1,75 @@
+from model import set_seeds, read_corpus, demojize_tweet, train_and_evaluate, write_results_to_file
+from wordsegment import load
+import tensorflow as tf
+import pandas as pd
+
+offensive_words_df = pd.read_csv('../OffensiveWords.csv', header=None)
+offensive_words = set(offensive_words_df[0].str.strip().str.lower())
+
+
+def percentage_offensive_words(tweet):
+    """
+    Calculate the percentage of offensive words in the given tweet.
+
+    :param tweet: The tweet text to analyze.
+    :return: Percentage of offensive words in the tweet.
+    """
+    words = tweet.split()
+    if len(words) == 0:
+        return 0  # Avoid division by zero
+
+    offensive_count = sum(1 for word in words if word in offensive_words)
+    return offensive_count / len(words) * 100
+
+
+def main():
+    load()
+    set_seeds()
+
+    X_train, Y_train = read_corpus('../train.tsv')
+    X_dev, Y_dev = read_corpus('../dev.tsv')
+    X_train = [demojize_tweet(tweet) for tweet in X_train]
+    X_dev = [demojize_tweet(tweet) for tweet in X_dev]
+
+    # Run the model without extra features
+    report, model_name = train_and_evaluate(
+        "roberta-base",
+        X_train,
+        Y_train,
+        X_dev,
+        Y_dev,
+        64,
+        5e-6,
+        128,
+        4,
+        []
+    )
+    write_results_to_file('offensive_words_results.txt', report, model_name)
+
+    # Run the model with extra features to compare the results
+    extra_features = [
+        {
+            'function': percentage_offensive_words,
+            'name': 'offensive_percentage',
+            'dtype': tf.float32
+        }
+    ]
+
+    report, model_name = train_and_evaluate(
+        "roberta-base",
+        X_train,
+        Y_train,
+        X_dev,
+        Y_dev,
+        64,
+        5e-6,
+        128,
+        4,
+        extra_features
+    )
+    model_name += '\nThis model uses a set of offensive words to determine the percentage of offensive words in the tweet and use it as a new feature.'
+    write_results_to_file('offensive_words_results.txt', report, model_name)
+
+
+if __name__ == '__main__':
+    main()
